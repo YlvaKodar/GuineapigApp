34ac778d7fdcf7a1f289ebc8c78596884a2c4d40
Ylva Fröjmark <ylva.frojamrk@gmail.com>
2026-04-07 18:20:54 +0200

    New structure and added Maven wrapper

--- GuineapigApp/src/main/java/StartGuineaGame.java
+++
@@ -1,9 +0,0 @@
-public class StartGuineaGame{
-
-    public static void main (String [] args){
-
-        GuineaChallenge game = new GuineaChallenge();
-        game.start();
-
-    }
-}
---
+++ src/main/java/StartGuineaGame.java
@@ -0,0 +1,9 @@
+public class StartGuineaGame{
+
+    public static void main (String [] args){
+
+        GuineaChallenge game = new GuineaChallenge();
+        game.start();
+
+    }
+}
